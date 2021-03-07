c91d6f688d9bf23cfa4b1d0d7e4c8eaf799e1305
Danny Marco <marco.denny7@gmail.com>
2021-03-08 02:21:29 +0600

    Create project

---
+++ University/University/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace University.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
